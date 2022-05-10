353beb3a0362cf3dd1d03c4728124d0226118c70
Tom Wieland <tom.wieland@gmail.com>
2022-05-10 23:13:41 +0200

    add env var

--- server.ts
+++ server.ts
@@ -1,3 +1,8 @@
-import ultra from "https://deno.land/x/ultra@v1.0.1/server.ts";
+import ultra from 'https://deno.land/x/ultra@v1.0.1/server.ts';
+
+const FAUNADB_KEY = Deno.env.get('FAUNADB_KEY');
+if (!FAUNADB_KEY || FAUNADB_KEY.length === 0) {
+  throw new Error('Environment variable FAUNADB_KEY is not set');
+}
 
 await ultra();
