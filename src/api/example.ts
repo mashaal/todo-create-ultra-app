3e0d2fd0756fec8bfa027fdad554b03a56a4fa3b
Tom Wieland <tom.wieland@gmail.com>
2022-05-10 22:49:09 +0200

    refactor(app): appy linting

--- src/api/example.ts
+++ src/api/example.ts
@@ -1,4 +1,4 @@
 export default () => {
-  const body = JSON.stringify({ msg: "HERE COMES THE FUTURE" });
+  const body = JSON.stringify({ msg: 'HERE COMES THE FUTURE' });
   return new Response(body);
 };
--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { SWRConfig } from "swr";
-import { Helmet } from "react-helmet";
-import { Route, Switch } from "wouter";
-import ultraCache from "ultra/cache";
-import { Cache } from "https://deno.land/x/ultra/src/types.ts";
+import React from 'react';
+import { SWRConfig } from 'swr';
+import { Helmet } from 'react-helmet';
+import { Route, Switch } from 'wouter';
+import ultraCache from 'ultra/cache';
+import { Cache } from 'https://deno.land/x/ultra/src/types.ts';
 
 const options = (cache: Cache) => ({
   provider: () => ultraCache(cache),
@@ -15,11 +15,11 @@ const Ultra = ({ cache }: { cache: Cache }) => {
     <SWRConfig value={options(cache)}>
       <Helmet>
         <title>Ultra</title>
-        <link rel="stylesheet" href="/style.css" />
+        <link rel='stylesheet' href='/style.css' />
       </Helmet>
       <main>
         <Switch>
-          <Route path="/">
+          <Route path='/'>
             <h1>@__@</h1>
           </Route>
           <Route>
