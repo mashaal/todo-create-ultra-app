61889ebb47ee04e4956afb50181c82e107f4e374
Tom Wieland <tom.wieland@gmail.com>
2022-05-11 08:59:59 +0200

    feature(app): use graphql endpoint directly from SSR

--- src/lib/graphql.ts
+++ src/lib/graphql.ts
@@ -1,10 +1,16 @@
 import { QueryClient } from 'react-query';
 import { GraphQLClient } from 'graphql-request';
+import { isServer } from './environment.ts';
 
 const root = Deno.env.get('root') || 'http://localhost:8000';
 
 export const endpoint = `${root}/api/graphql`;
 
 export const client = new GraphQLClient(endpoint);
+
+if (isServer()) {
+  client.setEndpoint('https://graphql.fauna.com/graphql');
+  client.setHeader('Authorization', `Bearer ${Deno.env.get('FAUNADB_KEY')}`);
+}
 
 export const queryClient = new QueryClient();
